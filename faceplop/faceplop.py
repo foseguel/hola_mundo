2f6934aa965ee717eaae14b5f0091b67b5da7e6d
foseguel <31484225+foseguel@users.noreply.github.com>
2017-08-30 18:06:10 -0300

    prueba1
    prueba de validacion 1

---
+++ faceplop/faceplop.py
@@ -0,0 +1,44 @@
+print("Welcome to the ....")
+
+print("""
+
+___________                          .__                 
+\_   _____/____    ____  ____ ______ |  |   ____ ______  
+ |    __) \__  \ _/ ___\/ __ \\____ \|  |  /  _ \\____ \ 
+ |     \   / __ \\  \__\  ___/|  |_> >  |_(  <_> )  |_> >
+ \___  /  (____  /\___  >___  >   __/|____/\____/|   __/ 
+     \/        \/     \/    \/|__|               |__|    
+
+""")
+
+
+
+
+nombre=input("Hola!!...cual es tu nombre?")
+print()
+print("Que tal", nombre)
+nacimiento=int(input("dime en que año naciste por favor   "))
+print()
+print("perfecto!..eso quiere decir que tienes",(2017-nacimiento), "años   ")
+print()
+estatura_m=float(input("Necesito otro dato mas, dime tu estatura en metros   "))
+print()
+estatura_c=int(estatura_m*100)
+print("tu estatura en centimetros es", estatura_c, "cms")
+print()
+print()
+num_amig=int(input("por ultimo dime la cantidad de amigos que tienes   "))
+print()
+print("muy bien, gracias por tus respuestas, en base a eso podemos decir lo siguiente:  ")
+print()
+print("Tu nombre es", nombre,)
+print("tienes", (2017-nacimiento), "años")
+print("tu estatura en cms es de", estatura_c)
+print("y tienes", num_amig, "amigos a quienes puedes invitar a este sitio")
+print()
+print()
+mensaje=input("Ahora publicaremos tu primer mensaje..... que piensas hoy?..." )
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+print(nombre, "piensa que.....", mensaje)
+print("---------------------------------------------------------")
+print("---------------------------------------------------------")
